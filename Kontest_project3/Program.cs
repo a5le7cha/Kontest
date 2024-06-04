7155f5c358183172ba73322c6a2bf27d3fcb7e65
eremushk <eremeevlasha@yandex.ru>
2024-06-04 14:10:01 +0300

    Task 4

--- Kontest_project3/Program.cs
+++ Kontest_project3/Program.cs
@@ -1,37 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Kontest_project3
 {
     class Program
     {
-        private static int GetMinDistance(int[] distance, bool[] shortestPathTreeSet, int verticesCount)
-        {
-            int min = int.MaxValue;
-            int minIndex = 0;
-
-            for (int v = 0; v < verticesCount; v++)
-            {
-                if (!shortestPathTreeSet[v] && distance[v] <= min)
-                {
-                    min = distance[v];
-                    minIndex = v;
-                }
-            }
-
-            return minIndex;
-        }
-
         public static int Dijkstra(int[,] graph, int source, int verticesCount)
         {
-            int[] distance = new int[verticesCount];
-            bool[] shortestPathTreeSet = new bool[verticesCount];
+            int[] distance = new int[verticesCount+1];
+            bool[] shortestPathTreeSet = new bool[verticesCount+1];
 
-            for (int i = 0; i < verticesCount; i++)
+            for (int i = 1; i <= verticesCount; i++)
             {
                 distance[i] = int.MaxValue;
                 shortestPathTreeSet[i] = false;
@@ -39,14 +19,26 @@ namespace Kontest_project3
 
             distance[source] = 0;
 
-            for (int count = 0; count < verticesCount - 1; count++)
+            for (int count = 1; count <= verticesCount; count++)
             {
-                int u = GetMinDistance(distance, shortestPathTreeSet, verticesCount);
+                int min = int.MaxValue;
+                int minIndex = 0;
+
+                for (int v = 1; v <= verticesCount; v++)
+                {
+                    if (!shortestPathTreeSet[v] && distance[v] < min)
+                    {
+                        min = distance[v];
+                        minIndex = v;
+                    }
+                }
+
+                int u = minIndex;
                 shortestPathTreeSet[u] = true;
 
-                for (int v = 0; v < verticesCount; v++)
+                for (int v = 1; v <= verticesCount; v++)
                 {
-                    if (!shortestPathTreeSet[v] && graph[u, v] != 0 && distance[u] != int.MaxValue && distance[u] + graph[u, v] < distance[v])
+                    if (!shortestPathTreeSet[v] && graph[u, v] != int.MaxValue && distance[u] + graph[u, v] < distance[v])
                     {
                         distance[v] = distance[u] + graph[u, v];
                     }
@@ -54,7 +46,7 @@ namespace Kontest_project3
             }
 
             int max = int.MinValue;
-            for(int i = 0; i<distance.Length; i++)
+            for(int i = 1; i<distance.Length; i++)
             {
                 if (distance[i] > max) max = distance[i];
             }
@@ -64,7 +56,80 @@ namespace Kontest_project3
 
         static void Main(string[] args)
         {
-            //нужно списать и там где ребра не указаны сделать их равными 1, достроить граф до клики
+            int[,] graph;
+            List<KeyValuePair<int, int>> roads = new List<KeyValuePair<int, int>>();
+            int N = 0;
+
+            using (var rd = new StreamReader("input.txt"))
+            {
+                string[] lin = rd.ReadLine().Split(' ');
+
+                N = int.Parse(lin[0]);
+                int M = int.Parse(lin[1]);
+
+                graph = new int[N+1, N+1];
+
+                for (int i = 0; i < M; i++)
+                {
+                    string[] line = rd.ReadLine().Split(' ');
+
+                    int city1 = int.Parse(line[0]);
+                    int city2 = int.Parse(line[1]);
+
+                    roads.Add(new KeyValuePair<int, int>(city1, city2));
+                }
+            }
+
+            for (int i = 1; i < graph.GetLength(0); i++)
+            {
+                for (int j = 1; j < graph.GetLength(1); j++)
+                {
+                    graph[i, j] = int.MaxValue;
+                }
+            }
+
+            for (int i = 0; i<roads.Count; i++)
+            {
+                graph[roads[i].Key, roads[i].Value] = 0;
+
+                if(!roads.Contains(new KeyValuePair<int, int>(roads[i].Value, roads[i].Key)))
+                    graph[roads[i].Value, roads[i].Key] = 1;
+            }
+
+            //for (int i = 1; i< graph.GetLength(0); i++)
+            //{
+            //    for(int j = 1; j < graph.GetLength(1); j++)
+            //    {
+            //        if (roads.Contains(new KeyValuePair<int, int>(i, j)))
+            //        {
+            //            graph[i, j] = 0;
+
+            //            if(!roads.Contains(new KeyValuePair<int, int>(j, i)))
+            //                graph[j, i] = 1;
+            //        }
+            //        //else if (i == j)
+            //        //{
+            //        //    graph[i, j] = 0;
+            //        //    graph[j, i] = 0;
+            //        //}
+            //        //else if (graph[i, j] != 0 && graph[j, i] != 0)
+            //        //{
+            //        //    graph[i, j] = 0;
+            //        //    graph[j, i] = 0;
+            //        //}
+            //    }
+            //}
+
+            int max = int.MinValue;
+            for(int i = 1; i<=N; i++)
+            {
+                var num = Dijkstra(graph, i, N);
+                if(max < num) { max = num; }
+            }
+
+            Console.WriteLine(max);
+
+            Console.ReadKey();
         }
     }
 }
